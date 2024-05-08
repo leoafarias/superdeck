500c7adb03e1b0ce1f9d8ce51531c782f2ab9dbb
Leo Farias <leoafarias@gmail.com>
2024-05-08 14:59:33 -0400

    analyze

--- test/builder/slides_loader_test.dart
+++ test/builder/slides_loader_test.dart
@@ -1,15 +1,14 @@
-import 'package:test/test.dart'; 
+import 'package:flutter_test/flutter_test.dart';
 import 'package:superdeck/builder/slides_loader.dart';
 
 void main() {
-	group(
-		SlidesLoader, 
-		() {
-			test(
-				'',
-				() async {
-				},
-			);
-		},
-	);
+  group(
+    SlidesLoader,
+    () {
+      test(
+        '',
+        () async {},
+      );
+    },
+  );
 }
